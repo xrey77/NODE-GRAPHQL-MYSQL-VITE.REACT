3eb2d46e8eb8f560a84efd7e8a6d02d5eb8b7296
Reynald Marquez-Gragasin <rey107@gmail.com>
2026-02-02 20:13:08 +0800

    NodeJS-Typescript, GraphQL, Vite+React and Mysql

--- node-graphql/src/config/db.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    HOST: process.env.DB_HOST || "localhost",
-    USER: process.env.DB_USER || "root",
-    PASSWORD: process.env.DB_PASSWORD || "rey",
-    DB: process.env.DB_NAME || "node_graphql",
-    PORT: parseInt(process.env.DB_PORT || "3306", 10),
-  };
-  
